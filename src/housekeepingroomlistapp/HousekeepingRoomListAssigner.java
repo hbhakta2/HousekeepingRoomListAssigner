05885bbb44e4576a67036fcf5b24fdafb52574a3
hbhakta2 <Professional@HBPC>
2025-06-12 15:44:55 -0500

    Fixed naming conventions

--- src/housekeepingroomlistapp/HousekeepingRoomListAssigner.java
+++ src/housekeepingroomlistapp/HousekeepingRoomListAssigner.java
@@ -17,7 +17,7 @@ public class HousekeepingRoomListAssigner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HousekeepingRoomListAssignmentFrame frame = new HousekeepingRoomListAssignmentFrame();
+        HousekeepingRoomListAssignmentFrame housekeepingRoomListAssignmentFrame = new HousekeepingRoomListAssignmentFrame();
     }
     
 }
--- src/housekeepingroomlistapp/HousekeepingRoomListAssignmentFrame.java
+++ src/housekeepingroomlistapp/HousekeepingRoomListAssignmentFrame.java
@@ -1,7 +1,6 @@
 package housekeepingroomlistapp;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GraphicsConfiguration;
